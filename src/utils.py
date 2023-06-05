08ba570fded6cd0d2395a99c6ba727f5b6db2427
Vladimir Semikov <voesem@ya.ru>
2023-06-05 12:34:33 +0300

    написана функция для форматирования номера счета или карты клиента

--- src/utils.py
+++ src/utils.py
@@ -34,3 +34,22 @@ def format_date_string(string):
     date_string = date_obj.strftime('%d.%m.%Y')
 
     return date_string
+
+
+def format_number(number):
+    """
+    Функция для форматирования номера счета (в формате **XXXX)
+    или карты клиента (в формате  XXXX XX** **** XXXX)
+    :param number: строка, содержащая неформатированный номер счета или карты
+    :return: возвращает номер счета (в формате **XXXX)
+    или карты клиента (в формате  XXXX XX** **** XXXX)
+    """
+    number = number.split(' ')
+
+    if len(number[-1]) == 20:
+        return f'{" ".join(number[:-1])} {"*" * 2 + number[-1][-4:]}'
+    elif len(number[-1]) == 16:
+        n = 4
+        hidden_number = number[-1][:6] + '*' * 6 + number[-1][-4:]
+        number_split = [hidden_number[j:j + n] for j in range(0, len(hidden_number), n)]
+        return f'{" ".join(number[:-1])} {" ".join(number_split)}'

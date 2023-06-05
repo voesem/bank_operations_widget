935bf960ceefbf1e093915877a62a3d084d662ca
Vladimir Semikov <voesem@ya.ru>
2023-06-05 12:45:14 +0300

    создан файл main.py, в котором реализована основная логика программы

---
+++ main.py
@@ -0,0 +1,13 @@
+from src.utils import get_last_operations, format_date_string, format_number
+
+# вывод сообщений о последних пяти успешных операций клиента
+for i in get_last_operations():
+    print(f'{format_date_string(i["date"])} {i["description"]}')
+
+    if 'from' not in i.keys():
+        print(f'{format_number(i["to"])}')
+    else:
+        print(f'{format_number(i["from"])} -> {format_number(i["to"])}')
+
+    print(f'{i["operationAmount"]["amount"]} {i["operationAmount"]["currency"]["name"]}')
+    print()

722604e21f16466edc2fbd1e0b5053e4dece80bb
Vladimir Semikov <voesem@ya.ru>
2023-06-05 12:37:35 +0300

    написан тест функции format_number

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-from src.utils import get_last_operations, format_date_string
+from src.utils import get_last_operations, format_date_string, format_number
 
 
 def test_get_last_operations():
@@ -15,3 +15,12 @@ def test_format_date_string():
     assert format_date_string('2019-08-26T10:50:58.294041') == '26.08.2019'
     assert format_date_string('2019-07-03T18:35:29.512364') == '03.07.2019'
     assert format_date_string('2018-06-30T02:08:58.425572') == '30.06.2018'
+
+
+def test_format_number():
+    """
+    Тест форматирования номера счета или карты
+    """
+    assert format_number('MasterCard 7158300734726758') == 'MasterCard 7158 30** **** 6758'
+    assert format_number('Visa Platinum 8990922113665229') == 'Visa Platinum 8990 92** **** 5229'
+    assert format_number('Счет 48894435694657014368') == 'Счет **4368'

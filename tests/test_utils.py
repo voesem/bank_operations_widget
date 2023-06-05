404b5eb61124cf8ded1c9b28353358ea3d2dbfef
Vladimir Semikov <voesem@ya.ru>
2023-06-05 12:29:33 +0300

    написан тест функции format_date_string

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-from src.utils import get_last_operations
+from src.utils import get_last_operations, format_date_string
 
 
 def test_get_last_operations():
@@ -6,3 +6,12 @@ def test_get_last_operations():
     Тест количества операций, которые выводит функция get_last_operations
     """
     assert len(get_last_operations()) == 5
+
+
+def test_format_date_string():
+    """
+    Тест форматирования даты операции
+    """
+    assert format_date_string('2019-08-26T10:50:58.294041') == '26.08.2019'
+    assert format_date_string('2019-07-03T18:35:29.512364') == '03.07.2019'
+    assert format_date_string('2018-06-30T02:08:58.425572') == '30.06.2018'

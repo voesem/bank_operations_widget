7516394ff9621699a884a3b5e1ed0d7b5eeb8548
Vladimir Semikov <voesem@ya.ru>
2023-06-06 08:59:15 +0300

    написан тест для функции чтения файла operations.json

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,44 @@
 from src.utils import load_operations, get_last_operations, format_date_string, format_number
 
 
+def test_load_operations():
+    """
+    Тест функции чтения файла operations.json
+    (на совпадение первых двух операций)
+    """
+    assert load_operations()[:2] == [
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {
+                "amount": "31957.58",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589"
+        },
+        {
+            "id": 41428829,
+            "state": "EXECUTED",
+            "date": "2019-07-03T18:35:29.512364",
+            "operationAmount": {
+                "amount": "8221.37",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "MasterCard 7158300734726758",
+            "to": "Счет 35383033474447895560"
+        }]
+
+
 def test_get_last_operations():
     """
     Тест количества операций, которые выводит функция get_last_operations
